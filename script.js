ce55f3b587ab892ce3130fd22fd48a7db347a638
Matthew McGrath <rubywep@hotmail.com>
2022-02-22 23:16:30 -0800

    Trim list of Tools. Create custom ::selection
    Trim list of tools to the most essential, removing all game development ones. Organized them into front/back-end categories. Added some minor transitions and underline animation.

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ function setNoun() {
   if (++nounIndex >= NOUNS.length) nounIndex = 0;
   nounElement.innerHTML = NOUNS[nounIndex];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
