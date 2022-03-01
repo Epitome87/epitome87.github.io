e494c92c422bbfba7d660505d218d501e08e8aac
Matthew McGrath <rubywep@hotmail.com>
2022-02-28 22:32:18 -0800

    Add ParticleJS effect. Change section colors. Tweak hero letter-spacing.

--- script.js
+++ script.js
@@ -21,24 +21,8 @@ function setNoun() {
   nounElement.innerHTML = NOUNS[nounIndex];
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function reveal() {
-  let reveals = document.querySelectorAll(".reveal");
+  let reveals = document.querySelectorAll('.reveal');
 
   for (let i = 0; i < reveals.length; i++) {
     let windowHeight = window.innerHeight;
@@ -46,11 +30,11 @@ function reveal() {
     let elementVisible = 150;
 
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
+      reveals[i].classList.add('active');
     } else {
-      reveals[i].classList.remove("active");
+      reveals[i].classList.remove('active');
     }
   }
 }
 
-window.addEventListener("scroll", reveal);
+window.addEventListener('scroll', reveal);
