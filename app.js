e61fe505746013e5126176858e50ca7fe0b39ff3
Matthew McGrath <rubywep@hotmail.com>
2022-04-17 16:08:33 -0700

    Further develop HTML & CSS

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ particlesJS('particles-js', {
   particles: {
     number: { value: 6 * 3, density: { enable: true, value_area: 800 } },
     // color: { value: '#33FCFF' },
-    color: { 
-      value: ['#a0e9ff', '#a162e8', '#f093b0', '#edca85']
+    color: {
+      value: ['#a0e9ff', '#a162e8', '#f093b0', '#edca85'],
     },
     shape: {
       type: ['polygon'],
-      stroke: { width: 0, color:  '#a0e9ff' },
+      stroke: { width: 0, color: '#a0e9ff' },
       polygon: { nb_sides: 9 },
       image: { src: '', width: 100, height: 100 },
     },
@@ -27,7 +27,7 @@ particlesJS('particles-js', {
       anim: { enable: true, speed: 10, size_min: 80, sync: false },
     },
     line_linked: {
-      enable: true,
+      enable: false,
       distance: 400,
       color: '#33FCFF',
       opacity: 0.5,
@@ -53,7 +53,13 @@ particlesJS('particles-js', {
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8 / 16, speed: 3 },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8 / 16,
+        speed: 3,
+      },
       repulse: { distance: 200, duration: 0.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 },
